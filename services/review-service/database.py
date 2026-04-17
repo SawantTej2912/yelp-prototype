f3ab193cd9e99152ef14f4cdab54f677fa1560c7
Shashira <g.shashira@gmail.com>
2026-04-16 19:54:37 -0700

    Set up MongoDB foundation for review service

--- services/review-service/database.py
+++ services/review-service/database.py
@@ -1,23 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
+from motor.motor_asyncio import AsyncIOMotorClient
 
-load_dotenv()
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "yelp_lab2")
 
-DB_URL = (
-    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-)
+client = AsyncIOMotorClient(MONGO_URL)
+db = client[MONGO_DB_NAME]
 
-engine = create_engine(DB_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+reviews_collection = db["reviews"]
+restaurants_collection = db["restaurants"]
